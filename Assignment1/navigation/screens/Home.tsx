f47a0f6e74e0ed2fa358a63cda1a2688f1d7ca37
Stefanie Haindl <11haindl@gmail.com>
2023-10-24 22:06:35 +0200

    Map incl. Markers and current Position

--- Assignment1/navigation/screens/Home.tsx
+++ Assignment1/navigation/screens/Home.tsx
@@ -1,21 +1,30 @@
-import * as React from 'react';
-import { View, Text, VirtualizedList, StyleSheet } from 'react-native';
-import Papa from 'papaparse';
-import StationList from '../../station-list/StationList';
-import { ScrollView } from 'react-native-gesture-handler';
+import * as React from "react";
+import {
+  View,
+  Text,
+  VirtualizedList,
+  StyleSheet,
+  Modal,
+  Alert,
+  Pressable,
+  TextInput,
+} from "react-native";
+import Papa from "papaparse";
 
 type CsvRow = {
-    HALTESTELLEN_ID: string;
-    NAME: string;
-    // Add more columns with their respective types as needed
-  };
+  HALTESTELLEN_ID: string;
+  NAME: string;
+  // Add more columns with their respective types as needed
+};
 
 export default function Home() {
-    const [data, setData] = React.useState<CsvRow[]>([]);
+  const [data, setData] = React.useState<CsvRow[]>([]);
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [station, setStation] = React.useState('');
 
   React.useEffect(() => {
     // Replace 'your_csv_url_here' with the actual URL of your CSV file.
-    fetch('https://data.wien.gv.at/csv/wienerlinien-ogd-haltestellen.csv')
+    fetch("https://data.wien.gv.at/csv/wienerlinien-ogd-haltestellen.csv")
       .then((response) => response.text())
       .then((csv) => {
         // Parse the CSV data using PapaParse
@@ -36,26 +45,121 @@ export default function Home() {
     );
   };
 
-    return (
-        <VirtualizedList
-      data={data}
-      initialNumToRender={20} // Adjust as needed
-      renderItem={renderItem}
-      keyExtractor={(item) => item.HALTESTELLEN_ID}
-      getItemCount={getItemCount}
-      getItem={getItem}
-    />
-    );
+  function addNewStation() {
+
+    const newId = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+    // Erstelle einen neuen Eintrag
+    const newStation: CsvRow = {
+      HALTESTELLEN_ID: newId.toString(), // Setze hier eine neue eindeutige ID
+      NAME: station, // Setze hier den Namen der neuen Haltestelle
+      // Füge weitere Spalten und ihre Werte hinzu, falls erforderlich
+    };
+
+    // Kopiere die vorhandenen Daten und füge den neuen Eintrag hinzu
+    const newData = [...data, newStation];
+
+    // Setze die aktualisierten Daten
+    setData(newData);
+    setModalVisible(!modalVisible)
+    console.log(newData);
+  }
+
+  return (
+    <>
+      <VirtualizedList
+        data={data}
+        initialNumToRender={20} // Adjust as needed
+        renderItem={renderItem}
+        keyExtractor={(item) => item.HALTESTELLEN_ID}
+        getItemCount={getItemCount}
+        getItem={getItem}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text>Stationsname:</Text>
+            <TextInput 
+              style={styles.input}
+              onChangeText={setStation}
+              value={station}
+            />
+            <Pressable
+              style={[styles.button]}
+              onPress={addNewStation}
+            >
+              <Text style={styles.textStyle}>Station hinzufügen</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    item: {
-      borderColor: 'black',
-      borderWidth: 1,
-      borderRadius: 8,
-      padding: 8
+  item: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+  },
+  itemText: {
+    fontSize: 18,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    itemText: {
-        fontSize: 18
-    }
-  });
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "dodgerblue"
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  input: {
+    width: 100,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
--- Assignment1/navigation/screens/Map.tsx
+++ Assignment1/navigation/screens/Map.tsx
@@ -1,16 +1,113 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import Papa from "papaparse";
+import * as Location from "expo-location";
+
+interface ILocation {
+  latitude: number;
+  longitude: number;
+}
 
 type MapProps = {
-    navigation: any;
-  };
+  navigation: any;
+};
+
+type CsvRow = {
+  HALTESTELLEN_ID: string;
+  NAME: string;
+  WGS84_LAT: string;
+  WGS84_LON: string;
+  // Weitere Spalten mit ihren jeweiligen Typen hinzufügen
+};
 
 export default function Map({ navigation }: MapProps) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Map Screen</Text>
-        </View>
-    );
+  const [markers, setMarkers] = React.useState<CsvRow[]>([]);
+  const [userLocation, setUserLocation] = React.useState<ILocation | undefined>(
+    undefined
+  );
+
+  // Standort abrufen und `expo-location` verwenden
+  const getUserLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Berechtigung verweigert");
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    setUserLocation({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+  };
+
+  React.useEffect(() => {
+    getUserLocation();
+    console.log(userLocation);
+
+    // CSV-Daten abrufen und parsen
+    fetch("https://data.wien.gv.at/csv/wienerlinien-ogd-haltestellen.csv")
+      .then((response) => response.text())
+      .then((csv) => {
+        const parsedData = Papa.parse(csv, { header: true }).data as CsvRow[];
+        setMarkers(parsedData);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {userLocation && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 48.2395854,
+            longitude: 16.3743125,
+            latitudeDelta: 0.0522,
+            longitudeDelta: 0.0321,
+          }}
+        >
+          {markers.map((marker, index) => {
+            const latitude = parseFloat(marker.WGS84_LAT);
+            const longitude = parseFloat(marker.WGS84_LON);
+
+            if (!isNaN(latitude) && !isNaN(longitude)) {
+              return (
+                <Marker
+                  key={index}
+                  coordinate={{
+                    latitude,
+                    longitude,
+                  }}
+                  title={marker.NAME}
+                  description={marker.HALTESTELLEN_ID}
+                ></Marker>
+              );
+            }
+            return null;
+          })}
+
+          {userLocation && (
+            <Marker
+              coordinate={{
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+              }}
+              title="Dein Standort"
+              pinColor="blue"
+            />
+          )}
+        </MapView>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
