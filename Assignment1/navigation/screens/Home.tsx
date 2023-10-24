ae36053830ecb7902b20e0bc8f2cd26ee12806ad
Stefanie Haindl <11haindl@gmail.com>
2023-10-24 22:55:10 +0200

    cleaned Home-Page

--- Assignment1/navigation/screens/Home.tsx
+++ Assignment1/navigation/screens/Home.tsx
@@ -5,38 +5,58 @@ import {
   VirtualizedList,
   StyleSheet,
   Modal,
-  Alert,
   Pressable,
   TextInput,
 } from "react-native";
 import Papa from "papaparse";
 
-type CsvRow = {
+// Definition of object "Station"
+type Station = {
+  // Properties of object - names are as they are in the csv-file
   HALTESTELLEN_ID: string;
   NAME: string;
-  // Add more columns with their respective types as needed
 };
 
 export default function Home() {
-  const [data, setData] = React.useState<CsvRow[]>([]);
+  const [data, setData] = React.useState<Station[]>([]);
   const [modalVisible, setModalVisible] = React.useState(false);
-  const [station, setStation] = React.useState('');
+  const [station, setStation] = React.useState("");
 
   React.useEffect(() => {
-    // Replace 'your_csv_url_here' with the actual URL of your CSV file.
+    // fetch data from csv
     fetch("https://data.wien.gv.at/csv/wienerlinien-ogd-haltestellen.csv")
       .then((response) => response.text())
       .then((csv) => {
-        // Parse the CSV data using PapaParse
-        const { data } = Papa.parse<CsvRow>(csv, { header: true });
+        // Parse csv data to get an array of Station objects
+        const { data } = Papa.parse<Station>(csv, { header: true });
         setData(data);
       });
   }, []);
 
-  const getItemCount = () => data.length;
-  const getItem = (data: CsvRow[], index: number) => data[index];
+  function addNewStation() {
+    // create a random 8-digit number as id of the new Station
+    const newId =
+      Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+    // create a new Station Object
+    const newStation: Station = {
+      HALTESTELLEN_ID: newId.toString(),
+      NAME: station,
+    };
+    // copy data to new array and add the created Station
+    const newData = [...data, newStation];
+    // Updata data
+    setData(newData);
+    //close Modal
+    setModalVisible(!modalVisible);
+    console.log(newData);
+  }
 
-  const renderItem = ({ item }: { item: CsvRow }) => {
+  // get number of items that should be displayed
+  const getItemCount = () => data.length;
+
+  const getItem = (data: Station[], index: number) => data[index];
+
+  const renderItem = ({ item }: { item: Station }) => {
     return (
       <View style={styles.item}>
         <Text style={styles.itemText}>{item.NAME}</Text>
@@ -45,65 +65,42 @@ export default function Home() {
     );
   };
 
-  function addNewStation() {
-
-    const newId = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
-    // Erstelle einen neuen Eintrag
-    const newStation: CsvRow = {
-      HALTESTELLEN_ID: newId.toString(), // Setze hier eine neue eindeutige ID
-      NAME: station, // Setze hier den Namen der neuen Haltestelle
-      // Füge weitere Spalten und ihre Werte hinzu, falls erforderlich
-    };
-
-    // Kopiere die vorhandenen Daten und füge den neuen Eintrag hinzu
-    const newData = [...data, newStation];
-
-    // Setze die aktualisierten Daten
-    setData(newData);
-    setModalVisible(!modalVisible)
-    console.log(newData);
-  }
-
   return (
     <>
       <VirtualizedList
-        data={data}
-        initialNumToRender={20} // Adjust as needed
-        renderItem={renderItem}
-        keyExtractor={(item) => item.HALTESTELLEN_ID}
-        getItemCount={getItemCount}
-        getItem={getItem}
+        data={data} // Data used for displaying
+        initialNumToRender={10} // Number of items rendered at the beginning
+        renderItem={renderItem} // single Item of the List
+        keyExtractor={(item) => item.HALTESTELLEN_ID} //unique key for each component
+        getItemCount={getItemCount} // total number of list entries
+        getItem={getItem} // get data of items that will be rendered in renderItem
       />
       <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
+        animationType="slide" // slide from bottom when Modal opens
+        transparent={true} // Background of Modal is transparent
+        visible={modalVisible} // if modalVisible = true Modal is open, if not it is closed
         onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
+          // function called when closing the modal
           setModalVisible(!modalVisible);
         }}
       >
+        {/* Content of the modal */}
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Text>Stationsname:</Text>
-            <TextInput 
+            <TextInput
               style={styles.input}
               onChangeText={setStation}
               value={station}
             />
-            <Pressable
-              style={[styles.button]}
-              onPress={addNewStation}
-            >
+            <Pressable style={[styles.button]} onPress={addNewStation}>
               <Text style={styles.textStyle}>Station hinzufügen</Text>
             </Pressable>
           </View>
         </View>
       </Modal>
-      <Pressable
-        style={[styles.button]}
-        onPress={() => setModalVisible(true)}
-      >
+      {/* Button for opening Modal and add a new Station */}
+      <Pressable style={[styles.button]} onPress={() => setModalVisible(true)}>
         <Text style={styles.textStyle}>Show Modal</Text>
       </Pressable>
     </>
@@ -144,7 +141,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 10,
     elevation: 2,
-    backgroundColor: "dodgerblue"
+    backgroundColor: "dodgerblue",
   },
   textStyle: {
     color: "white",
