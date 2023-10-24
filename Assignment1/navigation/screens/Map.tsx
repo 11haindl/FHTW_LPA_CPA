0fc374ba68f9ca8e136824d36fad948fc5aea6d5
Stefanie Haindl <11haindl@gmail.com>
2023-10-24 23:21:53 +0200

    clean Map Page

--- Assignment1/navigation/screens/Map.tsx
+++ Assignment1/navigation/screens/Map.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import Papa from "papaparse";
 import * as Location from "expo-location";
